df52931909c1b769f363d105f648663bd9039d16
Spencer K Chuong <spencerchuong0@gmail.com>
2024-02-21 14:53:48 -0800

    Buttons are functional

--- app/src/main/java/com/example/myapplication/BeginningActivity.kt
+++ app/src/main/java/com/example/myapplication/BeginningActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class BeginningActivity : AppCompatActivity() {
@@ -16,5 +18,21 @@ class BeginningActivity : AppCompatActivity() {
         animationDrawable.setEnterFadeDuration(2500)
         animationDrawable.setExitFadeDuration(5000)
         animationDrawable.start()
+        buttonFunc()
+    }
+    private
+    fun buttonFunc(){
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            // Intent to navigate to the SecondActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val signupbutton: Button = findViewById(R.id.sign_up_button)
+        signupbutton.setOnClickListener{
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }

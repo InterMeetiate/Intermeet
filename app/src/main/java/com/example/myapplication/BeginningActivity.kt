26c8540c05ae10e3b097ce53ef8e8094b1fa7ae7
Spencer K Chuong <spencerchuong0@gmail.com>
2024-02-26 22:28:02 -0800

    test connection to repo after transfer to Organization 2

--- app/src/main/java/com/example/myapplication/BeginningActivity.kt
+++ app/src/main/java/com/example/myapplication/BeginningActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.constraintlayout.widget.ConstraintLayout
-//hihi
+//hihihi
 class BeginningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

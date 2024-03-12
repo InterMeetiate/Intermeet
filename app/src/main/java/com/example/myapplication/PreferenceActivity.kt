e39b36db78aa1820f727e1ecc9df2457e5335676
Spencer K Chuong <spencerchuong0@gmail.com>
2024-03-11 23:55:16 -0700

    4/5 done

--- app/src/main/java/com/example/myapplication/PreferenceActivity.kt
+++ app/src/main/java/com/example/myapplication/PreferenceActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -117,6 +118,11 @@ class PreferenceActivity : AppCompatActivity(), DistanceFragment.OnDistanceSelec
         tvPolitics = findViewById(R.id.tvPolitics)
         tvPolitics.setOnClickListener {
             showPoliticsPicker()
+        }
+        backButton.setOnClickListener {
+            // Intent to navigate to the SecondActivity
+            val intent = Intent(this, DescriptionActivity::class.java)
+            startActivity(intent)
         }
 
     }

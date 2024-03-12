327d512588b8e9d979f1ef9a63bdc46d119c5381
Brian Huynh <bhuynh987@yahoo.com>
2024-03-11 21:41:51 -0700

    Added functionality to prompts page

--- app/src/main/java/com/example/myapplication/PromptsActivity.kt
+++ app/src/main/java/com/example/myapplication/PromptsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myapplication
 
+import CustomAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ImageView
@@ -13,6 +15,7 @@ class PromptsActivity : AppCompatActivity() {
     lateinit var promptTextbox: EditText
     lateinit var enter: ImageView
     lateinit var promptList: ArrayList<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prompts)
@@ -21,19 +24,27 @@ class PromptsActivity : AppCompatActivity() {
         promptTextbox = findViewById(R.id.enter_prompt)
         enter = findViewById(R.id.add)
 
-        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(this, android.R.layout.simple_list_item_1, promptList as List<String?>)
+        promptList = ArrayList()
 
+        val adapter = CustomAdapter(this, promptList)
         listView.adapter = adapter
 
         enter.setOnClickListener {
             val text = promptTextbox.text.toString()
-            if(text.isEmpty()) {
+            if (text.isEmpty()) {
                 Toast.makeText(this, "Please fill in the prompt.", Toast.LENGTH_SHORT).show()
-            }
-            else {
+            } else {
+                val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(promptTextbox.windowToken, 0)
+
+                // Adds textbox to list
                 promptList.add(text)
                 adapter.notifyDataSetChanged()
+
+                // Clear textbox after adding it to the list
+                promptTextbox.setText("")
             }
         }
     }
 }
+

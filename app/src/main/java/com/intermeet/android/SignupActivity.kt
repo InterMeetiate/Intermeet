3b3646ecb7e143506a81778d8ab99cdd1cd05b42
Alvin <alvinnguyen58@gmail.com>
2024-03-14 03:59:20 -0700

    Update SignupActivity.kt for UserDataRepository

--- app/src/main/java/com/intermeet/android/SignupActivity.kt
+++ app/src/main/java/com/intermeet/android/SignupActivity.kt
@@ -5,33 +5,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import com.intermeet.android.helperFunc.getUserDataRepository
 import java.lang.StringBuilder
 
 
-class SignupActivity : AppCompatActivity()
-{
-    override fun onCreate(savedInstanceState: Bundle?)
-    {
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         val sb = StringBuilder()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        val firstEdit : EditText = findViewById<EditText?>(R.id.signupUsername)
-        val lastEdit : EditText = findViewById(R.id.signupUsername2)
-        val firstName = firstEdit.text.toString()
-        val lastName = lastEdit.text.toString()
+        // Moved this to button func so that data is extracted when the button is selected
+//        val firstEdit: EditText = findViewById<EditText?>(R.id.signupUsername)
+//        val lastEdit: EditText = findViewById(R.id.signupUsername2)
 
-        sb.append(firstName).append(" ").append(lastName)
-        val entireName = sb.toString()
+//        val firstName = firstEdit.text.toString()
+//        val lastName = lastEdit.text.toString()
+
+//        sb.append(firstName).append(" ").append(lastName)
+//        val entireName = sb.toString()
 
         ButtonFunc()
     }
 
     private
-    fun ButtonFunc()
-    {
+    fun ButtonFunc() {
+        val firstEdit: EditText = findViewById(R.id.signupUsername)
+        val lastEdit: EditText = findViewById(R.id.signupUsername2)
+
+        val firstName = firstEdit.text.toString()
+        val lastName = lastEdit.text.toString()
+
+        // Retrieve userDataRepository
+        val userDataRepository = getUserDataRepository()
+        val userData = userDataRepository.userData ?: UserDataModel()
+
+        userData.firstName = firstName
+        userData.lastName = lastName
+
         val nextButton: Button = findViewById(R.id.next_button)
-        nextButton.setOnClickListener{
+        nextButton.setOnClickListener {
             val intent = Intent(this, EmailActivity::class.java)
             startActivity(intent)
         }

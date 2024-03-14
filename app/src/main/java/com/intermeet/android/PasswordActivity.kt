6aebc738911ae5dc3bc13ce2504f06fb93421616
Alvin <alvinnguyen58@gmail.com>
2024-03-14 03:59:59 -0700

    Update PasswordActivty.kt for UserDataRepository

--- app/src/main/java/com/intermeet/android/PasswordActivity.kt
+++ app/src/main/java/com/intermeet/android/PasswordActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.intermeet.android.helperFunc.getUserDataRepository
 
 class PasswordActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?)
@@ -12,15 +13,21 @@ class PasswordActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_password)
 
-        val passwordEdit :EditText = findViewById(R.id.password)
-        val password = passwordEdit.text.toString()
-
         ButtonFunc()
     }
 
     private
     fun ButtonFunc()
     {
+        val passwordEdit :EditText = findViewById(R.id.password)
+        val password = passwordEdit.text.toString()
+
+        // Retrieve userDataRepository
+        val userDataRepository = getUserDataRepository()
+        val userData = userDataRepository.userData ?: UserDataModel()
+
+        userData.password = password
+
         val nextButton: Button = findViewById(R.id.next_button)
         nextButton.setOnClickListener{
             val intent = Intent(this, BirthdayActivity::class.java)

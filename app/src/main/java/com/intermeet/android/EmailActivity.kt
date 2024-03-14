dfe5e6a3ebd531ec988c274f9d18f02e176c0d7d
Alvin <alvinnguyen58@gmail.com>
2024-03-14 03:59:39 -0700

    Update EmailActivity.kt for UserDataRepository

--- app/src/main/java/com/intermeet/android/EmailActivity.kt
+++ app/src/main/java/com/intermeet/android/EmailActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.intermeet.android.helperFunc.getUserDataRepository
 
 class EmailActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?)
@@ -12,8 +13,8 @@ class EmailActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_email)
 
-        val emailEdit : EditText = findViewById(R.id.email_input)
-        val email = emailEdit.text.toString()
+//        val emailEdit : EditText = findViewById(R.id.email_input)
+//        val email = emailEdit.text.toString()
 
         ButtonFunc()
     }
@@ -21,6 +22,15 @@ class EmailActivity : AppCompatActivity(){
     private
     fun ButtonFunc()
     {
+        val emailEdit : EditText = findViewById(R.id.email_input)
+        val email = emailEdit.text.toString()
+
+        // Retrieve userDataRepository
+        val userDataRepository = getUserDataRepository()
+        val userData = userDataRepository.userData ?: UserDataModel()
+
+        userData.email = email
+
         val nextButton: Button = findViewById(R.id.next_button)
         nextButton.setOnClickListener{
             val intent = Intent(this, PasswordActivity::class.java)

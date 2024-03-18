f9ee4f4344d946507699397d1558d0f439b0f2c5
Spencer K Chuong <spencerchuong0@gmail.com>
2024-03-18 10:29:35 -0700

    Template for Profile Page

---
+++ app/src/main/java/com/intermeet/android/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.intermeet.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile) // Sets the UI layout for this Activity.
+    }
+
+}

5dee911e0b801c440d5560598b60703cff0ac655
Spencer K Chuong <spencerchuong0@gmail.com>
2024-03-19 22:45:02 -0700

    Adding base for Profile Page and branch

---
+++ app/src/main/java/com/intermeet/android/ProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.intermeet.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}

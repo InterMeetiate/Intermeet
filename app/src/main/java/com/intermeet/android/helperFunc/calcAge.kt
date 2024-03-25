467808b5f3bd1ae5975969a72f4ebbbeef5634fd
Alvin <alvinnguyen58@gmail.com>
2024-03-25 09:23:51 -0700

    Create Discover Fragment

---
+++ app/src/main/java/com/intermeet/android/helperFunc/calcAge.kt
@@ -0,0 +1,23 @@
+package com.intermeet.android.helperFunc
+
+import java.text.SimpleDateFormat
+import java.util.*
+fun calculateAgeWithCalendar(birthDateString: String): Int {
+    val dateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
+    // Parse the birthdate string into a Date object
+    val birthDate = dateFormat.parse(birthDateString)
+    birthDate?.let {
+        val birthCalendar = Calendar.getInstance()
+        birthCalendar.time = it
+
+        val todayCalendar = Calendar.getInstance()
+
+        var age = todayCalendar.get(Calendar.YEAR) - birthCalendar.get(Calendar.YEAR)
+        if (todayCalendar.get(Calendar.DAY_OF_YEAR) < birthCalendar.get(Calendar.DAY_OF_YEAR)) {
+            age--
+        }
+
+        return age
+    }
+    return 0
+}

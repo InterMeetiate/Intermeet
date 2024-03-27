4230616827375eba43dc9f4a06661e82d7557d3c
slammedboi <sean_vw@yahoo.com>
2024-03-26 22:18:18 -0700

    first push to branch

--- app/src/main/java/com/intermeet/android/Home_Page/HomePage_Test.kt
+++ app/src/main/java/com/intermeet/android/Home_Page/HomePage_Test.kt
@@ -1,10 +1,11 @@
-package com.intermeet.android
+package com.intermeet.android.Home_Page
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.intermeet.android.R
 
 class HomePage_Test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

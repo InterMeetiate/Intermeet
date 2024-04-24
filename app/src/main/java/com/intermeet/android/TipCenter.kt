e2b5885695019277681ebec8508c22b46390af68
Spencer K Chuong <spencerchuong0@gmail.com>
2024-04-24 10:50:44 -0700

    Start of Tip center

---
+++ app/src/main/java/com/intermeet/android/TipCenter.kt
@@ -0,0 +1,11 @@
+package com.intermeet.android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TipCenter : AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tip_center)
+    }
+}

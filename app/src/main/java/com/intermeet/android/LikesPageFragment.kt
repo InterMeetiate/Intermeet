b4404c40e0364b31d068e865366604e407b69a3c
slammedBoi <105017472+slammedBoi@users.noreply.github.com>
2024-05-05 15:02:58 -0700

    test git

--- app/src/main/java/com/intermeet/android/LikesPageFragment.kt
+++ app/src/main/java/com/intermeet/android/LikesPageFragment.kt
@@ -39,8 +39,6 @@ class LikesPageFragment(private var recyclerDataArrayList: List<String>) : Fragm
                 val adapter = RecyclerViewAdapter(users, this, DiscoverActivity())
                 recyclerView.adapter = adapter
 
-                //val adapter = RecyclerViewAdapter(recyclerDataArrayList, this, DiscoverActivity())
-
                 // setting grid layout manager to implement grid view.
                 // in this method '2' represent/s number of columns to be displayed in grid view.
                 val layoutManager = GridLayoutManager(requireContext().applicationContext, 2)
@@ -63,49 +61,26 @@ class LikesPageFragment(private var recyclerDataArrayList: List<String>) : Fragm
             }
         }
 
-                // added data from arraylist to adapter class.
-                /*val adapter = RecyclerViewAdapter(recyclerDataArrayList, this, DiscoverActivity())
+        return view
+    }
 
-                // setting grid layout manager to implement grid view.
-                // in this method '2' represent/s number of columns to be displayed in grid view.
-                val layoutManager = GridLayoutManager(requireContext().applicationContext, 2)
-
-                // at last set adapter to recycler view.
-                recyclerView.layoutManager = layoutManager
-                recyclerView.adapter = adapter
-                userDetailAdapter = UserDetailAdapter(this)
-
-
-                adapter.setOnClickListener(object : RecyclerViewAdapter.OnClickDetect {
-                    override fun onClickDetect(position: Int, userId: String) {
-                        val likesDetailFragment = LikesDetailFragment.newInstance(userId)
-                        parentFragmentManager.beginTransaction()
-                            .replace(R.id.fragmentContainer, likesDetailFragment)
-                            .addToBackStack(null)  // Optional: Add transaction to the back stack
-                            .commit()
-                    }
-                })*/
-
-                return view
+    private fun fetchLikedUsers(userID: String, callback: (List<String>) -> Unit) {
+        val userRef = FirebaseDatabase.getInstance().getReference("users").child(userID).child("likes")
+        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val likedUserIds = snapshot.children.mapNotNull { it.key }
+                callback(likedUserIds)
             }
 
-                    private fun fetchLikedUsers(userID: String, callback: (List<String>) -> Unit) {
-                val userRef = FirebaseDatabase.getInstance().getReference("users").child(userID).child("likes")
-                userRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val likedUserIds = snapshot.children.mapNotNull { it.key }
-                        callback(likedUserIds)
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {
-                        Log.e("FetchLikedUsers", "Error fetching liked user IDs: ${error.message}")
-                        callback(emptyList()) // Return an empty list in case of error
-                    }
-                })
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("FetchLikedUsers", "Error fetching liked user IDs: ${error.message}")
+                callback(emptyList()) // Return an empty list in case of error
             }
+        })
+    }
 
     companion object {
-                @JvmStatic
-                fun newInstance() = LikesPageFragment(emptyList())
-            }
-        }
+        @JvmStatic
+        fun newInstance() = LikesPageFragment(emptyList())
+    }
+}

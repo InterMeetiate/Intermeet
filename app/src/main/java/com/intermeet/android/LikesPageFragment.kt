187dc70c93eca4cd2dbdb65f312c11073aa72f54
slammedBoi <105017472+slammedBoi@users.noreply.github.com>
2024-05-05 04:45:00 -0700

    retrieve users that like current user

--- app/src/main/java/com/intermeet/android/LikesPageFragment.kt
+++ app/src/main/java/com/intermeet/android/LikesPageFragment.kt
@@ -1,5 +1,3 @@
-
-
 //import com.intermeet.android.DiscoverFragment
 import android.os.Bundle
 import android.util.Log
@@ -19,9 +17,8 @@ import com.intermeet.android.LikesDetailFragment
 import com.intermeet.android.R
 import com.intermeet.android.UserDetailAdapter
 
-class LikesPageFragment : Fragment() {
+class LikesPageFragment(private var recyclerDataArrayList: List<String>) : Fragment() {
     private lateinit var recyclerView: RecyclerView
-    private val recyclerDataArrayList = ArrayList<String>()
     private lateinit var userDetailAdapter: UserDetailAdapter
 
     override fun onCreateView(
@@ -30,75 +27,85 @@ class LikesPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
+        //var recyclerDataArrayList: List<String>
         val view = inflater.inflate(R.layout.fragment_likespage, container, false)
         recyclerView = view.findViewById(R.id.idCourseRV)
 
+        //add users that like current users account and send to adapter
         val userId = FirebaseAuth.getInstance().currentUser?.uid
-        //val database = Firebase.database
-        //val userRef = database.getReference("users").child(userId.toString())
-
-        // Clear the list before adding items to avoid duplicates
-        /*if(recyclerDataArrayList.size > 0)
-        {//NEED TO CHANGE WAY OF CLEARING IT
-            for(i in recyclerDataArrayList)
-            {
-                recyclerDataArrayList.remove(i)
-            }
-        }*/
-        // added data to array list
-        //recyclerDataArrayList.add("Xqi01fgXQNMdzdQEQEVJ6iB4wDu2")
-        //recyclerDataArrayList.add("CPzvj0777RUav4ACjiiS6BECxm82")
-        //recyclerDataArrayList.add("DK6LQRJYmxarqxOHCdy4MKS63Pp2")
-        //userRef.addValueEventListener()
+        val userRef = FirebaseDatabase.getInstance().getReference("users").child(userId.toString()).child("likes")
         if (userId != null) {
-            fetchLikedUsers(userId) {
-                users -> recyclerDataArrayList
+            fetchLikedUsers(userId) { users ->
+                val adapter = RecyclerViewAdapter(users, this, DiscoverActivity())
+                recyclerView.adapter = adapter
+
+                //val adapter = RecyclerViewAdapter(recyclerDataArrayList, this, DiscoverActivity())
+
+                // setting grid layout manager to implement grid view.
+                // in this method '2' represent/s number of columns to be displayed in grid view.
+                val layoutManager = GridLayoutManager(requireContext().applicationContext, 2)
+
+                // at last set adapter to recycler view.
+                recyclerView.layoutManager = layoutManager
+                recyclerView.adapter = adapter
+                userDetailAdapter = UserDetailAdapter(this)
+
+
+                adapter.setOnClickListener(object : RecyclerViewAdapter.OnClickDetect {
+                    override fun onClickDetect(position: Int, userId: String) {
+                        val likesDetailFragment = LikesDetailFragment.newInstance(userId)
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainer, likesDetailFragment)
+                            .addToBackStack(null)  // Optional: Add transaction to the back stack
+                            .commit()
+                    }
+                })
             }
         }
 
-        // added data from arraylist to adapter class.
-        val adapter = RecyclerViewAdapter(recyclerDataArrayList, this, DiscoverActivity())
+                // added data from arraylist to adapter class.
+                /*val adapter = RecyclerViewAdapter(recyclerDataArrayList, this, DiscoverActivity())
 
-        // setting grid layout manager to implement grid view.
-        // in this method '2' represent/s number of columns to be displayed in grid view.
-        val layoutManager = GridLayoutManager(requireContext().applicationContext, 2)
+                // setting grid layout manager to implement grid view.
+                // in this method '2' represent/s number of columns to be displayed in grid view.
+                val layoutManager = GridLayoutManager(requireContext().applicationContext, 2)
 
-        // at last set adapter to recycler view.
-        recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = adapter
-        userDetailAdapter = UserDetailAdapter(this)
+                // at last set adapter to recycler view.
+                recyclerView.layoutManager = layoutManager
+                recyclerView.adapter = adapter
+                userDetailAdapter = UserDetailAdapter(this)
 
 
-        adapter.setOnClickListener(object : RecyclerViewAdapter.OnClickDetect {
-            override fun onClickDetect(position: Int, userId: String) {
-                val likesDetailFragment = LikesDetailFragment.newInstance(userId)
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragmentContainer, likesDetailFragment)
-                    .addToBackStack(null)  // Optional: Add transaction to the back stack
-                    .commit()
-            }
-        })
+                adapter.setOnClickListener(object : RecyclerViewAdapter.OnClickDetect {
+                    override fun onClickDetect(position: Int, userId: String) {
+                        val likesDetailFragment = LikesDetailFragment.newInstance(userId)
+                        parentFragmentManager.beginTransaction()
+                            .replace(R.id.fragmentContainer, likesDetailFragment)
+                            .addToBackStack(null)  // Optional: Add transaction to the back stack
+                            .commit()
+                    }
+                })*/
 
-        return view
-    }
-
-    private fun fetchLikedUsers(userID: String, callback: (List<String>) -> List<String>) {
-        val userRef = FirebaseDatabase.getInstance().getReference("users").child(userID).child("likes")
-        userRef.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val likedUserIds = snapshot.children.mapNotNull { it.key }
-                callback(likedUserIds)
+                return view
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("FetchLikedUsers", "Error fetching liked user IDs: ${error.message}")
-                callback(emptyList()) // Return an empty list in case of error
+                    private fun fetchLikedUsers(userID: String, callback: (List<String>) -> Unit) {
+                val userRef = FirebaseDatabase.getInstance().getReference("users").child(userID).child("likes")
+                userRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val likedUserIds = snapshot.children.mapNotNull { it.key }
+                        callback(likedUserIds)
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+                        Log.e("FetchLikedUsers", "Error fetching liked user IDs: ${error.message}")
+                        callback(emptyList()) // Return an empty list in case of error
+                    }
+                })
             }
-        })
-    }
 
     companion object {
-        @JvmStatic
-        fun newInstance() = LikesPageFragment()
-    }
-}
+                @JvmStatic
+                fun newInstance() = LikesPageFragment(emptyList())
+            }
+        }

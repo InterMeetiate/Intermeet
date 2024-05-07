dd7f9165a8131a7a9f3ffe82bd096a53e1f27a39
Alvin <alvinnguyen58@gmail.com>
2024-05-06 18:03:20 -0700

    Implement return

--- app/src/main/java/com/intermeet/android/DiscoverFragment.kt
+++ app/src/main/java/com/intermeet/android/DiscoverFragment.kt
@@ -1,5 +1,7 @@
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +14,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.intermeet.android.CardStackAdapter
 import com.intermeet.android.DiscoverViewModel
+import com.intermeet.android.LikeAnimation
+import com.intermeet.android.PassAnimation
 import com.intermeet.android.R
 import com.intermeet.android.UserDataModel
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
@@ -27,6 +31,7 @@ class DiscoverFragment : Fragment() {
     private lateinit var btnRefresh: Button
     private lateinit var returnButton: View
     private lateinit var progressBar: ProgressBar
+    private lateinit var manager: CardStackLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -41,6 +46,8 @@ class DiscoverFragment : Fragment() {
         setupViews(view)
         setupListeners()
         setupCardStackView()
+        addLikeAnimationFragment()
+        addPassAnimationFragment()
 
         viewModel.filteredUsers.observe(viewLifecycleOwner) { users ->
             progressBar.visibility = View.GONE
@@ -65,15 +72,18 @@ class DiscoverFragment : Fragment() {
         adapter = CardStackAdapter(requireContext(), listOf())
         cardStackView.adapter = adapter
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun setupListeners() {
         returnButton.setOnClickListener {
+            cardStackView.rewind()
         }
 
         btnRefresh.setOnClickListener {
             fetchUsers()
         }
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun fetchUsers() {
         progressBar.visibility = View.VISIBLE
@@ -103,24 +113,27 @@ class DiscoverFragment : Fragment() {
     private fun setupCardStackView() {
         val manager = CardStackLayoutManager(context, object : CardStackListener {
             override fun onCardDragging(direction: Direction, ratio: Float) {
-                // You can leave this empty or add some logging if you want
             }
 
+            @RequiresApi(Build.VERSION_CODES.O)
             override fun onCardSwiped(direction: Direction) {
-                // Handle card swipe if necessary
+                when (direction) {
+                    Direction.Right -> triggerLikeAnimation()
+                    Direction.Left -> triggerPassAnimation()
+                    else -> {}
+                }
             }
 
             override fun onCardRewound() {
-                // Handle rewind if necessary
             }
 
             override fun onCardCanceled() {
-                // Handle card cancel if necessary
             }
 
             override fun onCardAppeared(view: View, position: Int) {
-                // Fade in the card as it appears
-                view.animate().alpha(1.0f).setDuration(300).start()
+                Handler(Looper.getMainLooper()).postDelayed({
+                    view.animate().alpha(1.0f).setDuration(100).start()
+                }, 500)
             }
 
             override fun onCardDisappeared(view: View, position: Int) {
@@ -134,8 +147,45 @@ class DiscoverFragment : Fragment() {
 
         cardStackView.layoutManager = manager
         cardStackView.adapter = adapter
+
     }
 
+
+    private fun triggerLikeAnimation() {
+        val likeAnimationFragment =
+            childFragmentManager.findFragmentByTag("LikeAnimationFragment") as? LikeAnimation
+        likeAnimationFragment?.let {
+            it.animateLike()
+            it.toggleBackgroundAnimation()
+            Log.d("DiscoverFragment", "Animation triggered")
+        }
+    }
+
+    private fun addLikeAnimationFragment() {
+        val transaction = childFragmentManager.beginTransaction()
+        val likeFragment = LikeAnimation()
+        transaction.add(R.id.like_animation_container, likeFragment, "LikeAnimationFragment")
+        transaction.commit()
+        Log.d("DiscoverFragment", "Like animation fragment added")
+    }
+
+    private fun triggerPassAnimation() {
+        val passAnimationFragment =
+            childFragmentManager.findFragmentByTag("PassAnimationFragment") as? PassAnimation
+        passAnimationFragment?.let {
+            it.animatePass()
+            it.toggleBackgroundAnimation()
+            Log.d("DiscoverFragment", "Animation triggered")
+        }
+    }
+
+    private fun addPassAnimationFragment() {
+        val transaction = childFragmentManager.beginTransaction()
+        val passFragment = PassAnimation()
+        transaction.add(R.id.like_animation_container, passFragment, "PassAnimationFragment")
+        transaction.commit()
+        Log.d("DiscoverFragment", "Pass animation fragment added")
+    }
 
     companion object {
         fun newInstance() = DiscoverFragment()
